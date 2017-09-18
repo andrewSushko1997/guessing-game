a2af72f8e7099bf486e0bdeabb3cfa74b750db0e
andrewSushko1997 <andrewklopz@gmail.com>
2017-09-18 16:28:19 +0300

    100%

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minNumber = 0;
+    	this.maxNumber = 0;
+    	this.midNumber = 0;
+    }
 
     setRange(min, max) {
-
+    	this.minNumber = min;
+    	this.maxNumber = max;
     }
 
     guess() {
-
+    	
+    	return this.midNumber = Math.ceil((this.minNumber + this.maxNumber)/2);
     }
 
     lower() {
-
+    	this.maxNumber = this.midNumber; 
     }
 
     greater() {
-
+    	this.minNumber = this.midNumber;
     }
 }
 
